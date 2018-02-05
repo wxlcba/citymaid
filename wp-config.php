114db8af2798ae73a31cde75a9b26aa0f284dc8d
e <wxlcba@gmail.com>
2018-02-05 21:46:52 +1300

    the newer compile

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'maidcity');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
